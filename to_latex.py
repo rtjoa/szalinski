abc962232c89c17ea58792e508970388f0f8ea7e
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-04-14 10:23:40 -0700

    ugly formatting script

--- to_latex.py
+++ to_latex.py
@@ -1,32 +1,63 @@
+#!python3
 import pandas as pd
-df = pd.read_csv("out/aec-table2/table2.csv", index_col=False)
+import sys
+s = sys.argv[1] if len(sys.argv) > 1 else "out/aec-table2/table2.csv"
+print(s)
+print()
+df = pd.read_csv(s, index_col=False)
 
 df = df.rename(columns={col: col.strip() for col in df.columns})
-df = df[['Id', 'c_in', 'No CAD', 'c_out', 'Ruler']]
+
+INIT = 'Init'
+BASELINE = 'Baseline'
+SZ = 'Szalinski'
+RULER = "Szalinski w/ \\tool's CAD"
+
 df = df.rename(columns={
-    'c_in': 'Init',
-    'No CAD': 'Baseline',
-    'c_out': 'Szalinski',
-    'Ruler': "Szalinski w/ \\tool's CAD",
+    'c_in': INIT,
+    'No CAD': BASELINE,
+    'c_out': SZ,
+    'Ruler': RULER,
 })
 
 m = df.drop('Id', axis=1).mean()
 m['Id'] = 'Average'
 
-df = df.round(0)
-df = df.convert_dtypes()
+df = df._append(m, ignore_index=True)
 
-df = pd.concat([df, pd.DataFrame([m])])
+d = df.to_dict()
 
-s = df.to_latex(index=False, float_format="%.1f")
+from copy import deepcopy
+d2 = deepcopy(d)
+
+for idx, test in d['Id'].items():
+    for col in reversed([INIT, BASELINE, SZ, RULER]):
+        f = (lambda x: round(x, 1)) if test == 'Average' else (lambda x: round(x))
+        absolute = f(d[col][idx])
+        d[col][idx] = f"{round(100 * (1 - (d[col][idx] / d[INIT][idx])))}\\% ({absolute})"
+
+        if col in (RULER,) and d2[col][idx] == min(d2[SZ][idx], d2[RULER][idx]):
+            d[col][idx] = f"\\textbf{{{d[col][idx]}}}"
+
+df = df.astype(str)
+
+
+
+for col, d_ in d.items():
+    for idx, v in d_.items():
+        df.loc[idx, col] = v
+
+df = df[['Id', BASELINE, SZ, RULER]]
+
+
+s = df.to_latex(index=False)
 
 final = []
 for line in s.split('\n'):
     if 'Average' in line:
         final.append('\\midrule')
-        final.append(line)
-    else:
-        final.append(line.replace(".0", ""))
+    final.append(line)
 
+final = final[2:-3]
 print('\n'.join(final))
    
